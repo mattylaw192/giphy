a7ac0d31b9ffdcdfc16fdadbc6154be43fe1031a
Mattylaw192 <mattylaw192@gmail.com>
2018-10-27 17:20:50 -0400

    Updated giphy search

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,23 +1,24 @@
-var animals = ["Giraffe","Baboon","Dog"];
+var animals = ["Giraffe", "Baboon", "Dog"];
 
-
+console.log(animals);
 
 function displayButtons() {
 
     $("#animal-buttons").empty();
 
-    for (var i = 0; i < animals.length; i++){
+    for (var i = 0; i < animals.length; i++) {
         var a = $("<button>");
         a.addClass("animal");
         a.addClass("btn btn-success");
-        a.attr("data-name",animals[i]);
+        a.attr("data-name", animals[i]);
         a.text(animals[i]);
         $("#animal-buttons").append(a);
     }
 
 }
 
-$("#add-animal").on("click", function(event){
+//Display buttons & add buttons
+$("#add-animal").on("click", function (event) {
 
     event.preventDefault();
 
@@ -26,65 +27,55 @@ $("#add-animal").on("click", function(event){
     animals.push(animal);
 
     displayButtons();
-    
+
 
 
 })
 
 displayButtons();
 
-$("#animal").on("click",function buildQueryURL(){
+
+
+
+$("button").on("click", function buildQueryURL() {
     var animal = $(this).attr("data-name");
-    var queryURL = "http://api.giphy.com/v1/gifs/search?" + animal + "&api_key=47knbJVz9ApSdFnyQ8P7WGFSfASrNSck";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=47knbJVz9ApSdFnyQ8P7WGFSfASrNSck";
+    console.log(animal);
+    console.log(queryURL);
 
     $.ajax({
         URL: queryURL,
-        method: "GET"
-    }).then(function(response){
-        var imageURL = response.data.image_original_url;
+        method: "GET",
+        crossDomain: true,
+        dataType: 'html',
 
-        var animalImage = $("<img>");
+    })
+    
+    .then(function (response) {
+        var results = response.data;
+        console.log(results);
+        for (var i = 0; i < results.length; i++) {
 
-        animalImage.attr("src", imageURL);
-        catImage.attr("alt", "animal image");
-        $("images").prepend(animalImage);
+            if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+                var gifDiv = $("<div>");
+
+                var rating = results[i].rating;
+
+                var p = $("<p>").text("Rating: " + rating);
+
+                var animalImage = $("<img>");
+                animalImage.attr("src", results[i].images.fixed_height.url);
+
+                gifDiv.append(p);
+                gifDiv.append(animalImage);
+
+                $("#images").prepend(gifDiv);
+            }
+
+        }
+
     });
 
 
+
 });
-
-
-
-
-
-
-// $.ajax({
-//     URL: queryURL,
-//     Method: "GET"
-// })
-
-
-//     .then(function (response) {
-
-
-//         var imageUrl = response.data.image_original_url;
-
-
-//         // Creating and storing an image tag
-//         var gifImage = $("<img>");
-
-//         // Setting the catImage src attribute to imageUrl
-//         gifImage.attr("src", imageUrl);
-//         gifImage.attr("alt", "gif image");
-
-//         // Prepending the catImage to the images div
-//         $("#images").prepend(gifImage);
-//     })
-
-
-
-// $("#addImageButton").on("click", function () {
-
-    
-
-// })
